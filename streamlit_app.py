875bd7e692d5978f66120fa84f01e982aebab4a6
snehalatha82 <74038736+snehalatha82@users.noreply.github.com>
2022-09-06 13:05:44 +0530

    Update streamlit_app.py
    Multi selection of fruits enabled

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,13 @@ streamlit.header('\N{banana}My Mom''s Breakfast Menu\N{egg}')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket smoothie')
 
+# Load the data from the CSV to Pandas Dataframe
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+#set index to Fruit instead of default number
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# select the  fruit and list them
+streamlit.multiselect('Pick Some Fruits ',  list(my_fruit_list.index)
 streamlit.dataframe(my_fruit_list)
