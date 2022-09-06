5aee79761f187e304532b615b155ce5ac0b0bf46
snehalatha82 <74038736+snehalatha82@users.noreply.github.com>
2022-09-06 12:52:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('\N{banana}My Mom''s Breakfast Menu')
-streamlit.text('\N{blueberries}Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{blueberry}Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket smoothie')
 
