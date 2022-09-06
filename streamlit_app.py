cd54d27094a932091c97f865a2231af7e180ab9e
snehalatha82 <74038736+snehalatha82@users.noreply.github.com>
2022-09-06 12:59:36 +0530

    Update streamlit_app.py
    Typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-steamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
