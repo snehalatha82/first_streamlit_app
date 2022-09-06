d9830b51aa8a245750b5ce980ae1973f6cf8e7c6
snehalatha82 <74038736+snehalatha82@users.noreply.github.com>
2022-09-06 12:10:28 +0530

    Update streamlit_app.py
    Added additional code for BF menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit;
 
 streamlit.title('My parents new healthy dinner');
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach and Rocket smoothie');
+
