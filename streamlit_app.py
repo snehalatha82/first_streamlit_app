38a7b73426d008c253c3ec50cd73808a578d9167
snehalatha82 <74038736+snehalatha82@users.noreply.github.com>
2022-09-06 12:17:34 +0530

    Update streamlit_app.py
    Removed the colon - Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
-import streamlit;
+import streamlit
 
-streamlit.title('My parents new healthy dinner');
-streamlit.header('Breakfast Menu');
-streamlit.text('Omega 3 & Blueberry Oatmeal');
-streamlit.text('Kale, Spinach and Rocket smoothie');
+streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket smoothie')
 
